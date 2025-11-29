602f61ea3868d05cde96c662362cb79bc61a07c7
Jeric June Logan <loganjeric25@gmail.com>
2025-11-29 17:40:40 +0800

    initial setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+import logo from '../logo.svg'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
